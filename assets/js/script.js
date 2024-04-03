10b9704926867fff93036777f7261101c55c44d0
Cristioncmps <cristion.campos@opovodigital.com>
2024-04-03 17:10:43 -0300

    feat: Responsive UP / Menu ADD

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+  // ABRIR BOX MENU MOBILE
+  $('.mobile-menu').click(function() {
+      $('.container__menu--mobile').toggleClass('active');
+  });
+});
+
+
 let owl = $(".owl-carousel");
 owl.owlCarousel({
   center: true,
